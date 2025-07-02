f0a06c4d32a9f52782f8b6b3d90ab24ba81aad79
NullDev (Shadow) <chris@nulldev.org>
2025-07-02 07:50:28 +0200

    configs

---
+++ config/config.template.js
@@ -0,0 +1,17 @@
+export default {
+    discord: {
+        bot_token: "",
+        bot_owner_ids: [],
+    },
+    roles: {
+        verified: "",
+        ages: {
+            "14_17": "",
+            "18_24": "",
+            "25_29": "",
+            "30_35": "",
+            "36_39": "",
+            "40+": "",
+        },
+    },
+};
