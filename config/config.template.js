7d1b73b2b26b59e02141d16cfb6095f69828e211
NullDev (Shadow) <chris@nulldev.org>
2025-07-05 16:33:09 +0200

    add openai dept

--- config/config.template.js
+++ config/config.template.js
@@ -24,4 +24,9 @@ export default {
     channels: {
         general: "",
     },
+    openai: {
+        token: "",
+        // "gpt-4" | "gpt-3.5-turbo" | "gpt-4-32k-0613" | "gpt-4-0314" | "gpt-4-0613" | "gpt-4-32k" | "gpt-4-32k-0314" | "gpt-3.5-turbo-16k" | "gpt-3.5-turbo-0301" | "gpt-3.5-turbo-0613" | "gpt-3.5-turbo-16k-0613
+        model: "gpt-4",
+    },
 };
