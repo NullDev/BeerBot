8bd8dc631916ed498dcd8c09d250630aabf619a0
NullDev (Shadow) <chris@nulldev.org>
2025-09-14 22:13:11 +0200

    add other country role to verification

--- config/config.template.js
+++ config/config.template.js
@@ -31,6 +31,7 @@ export default {
             kt: "",
             tr: "",
             vb: "",
+            other: "",
         },
         country_unverified: {
             oo: "",
@@ -42,6 +43,7 @@ export default {
             kt: "",
             tr: "",
             vb: "",
+            other: "",
         },
     },
     channels: {
