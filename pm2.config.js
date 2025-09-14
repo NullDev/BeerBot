6dfd2a5921363b9d5d2269f787af9525d03ad19f
NullDev (Shadow) <chris@nulldev.org>
2025-09-14 21:22:28 +0200

    add auto deploy scripts

---
+++ pm2.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "beerbot",
+    cwd: "./",
+    exec_mode: "fork",
+    instances: 1,
+    script: "src/app.js",
+    interpreter: "bun",
+    env: {
+        PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+    },
+    repo: "https://github.com/NullDev/BeerBot.git",
+    ref: "origin/master",
+};
