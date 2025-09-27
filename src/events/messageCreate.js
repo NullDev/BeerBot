4247080c0363cc9a30c0f135d03f81f90f41470a
NullDev (Shadow) <chris@nulldev.org>
2025-09-27 21:24:16 +0200

    hope that works. didnt test it LOL

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -16,6 +16,14 @@ await brain.init();
 const aiWorker = new PythonAIWorker();
 
 /**
+ * Clean message content by removing mentions and trimming whitespace
+ *
+ * @param {import("discord.js").Message} message
+ */
+const cleanMsg = message => message.cleanContent.replace(/<a?(:[a-zA-Z0-9_]+:)[0-9]+>/g, "$1")
+    .replace(`<@${message.client.user.id}>`, "").trim();
+
+/**
  * Handle messageCreate event
  *
  * @param {import("discord.js").Message} message
@@ -30,9 +38,11 @@ const messageCreateHandler = async function(message){
     else if (message.channel.type === ChannelType.GuildText && !message.author.bot){
         await jokes(message);
 
-        const channelId = message.channel.id; // @ts-ignore
-        if (config.discord.bot_owner_ids.includes(message.author.id) && message.content?.trim().startsWith("!!MKDEV ")){
-            const query = message.content.trim().substring(8).trim();
+        const channelId = message.channel.id;
+        if (channelId === "1421575808702484530" && message.mentions.has(message.client.user)){
+            if (message.content.trim() === `<@!${message.client.user?.id}>`) return;
+            const query = cleanMsg(message);
+            message.channel.sendTyping();
             try {
                 const reply = await aiWorker.infer(query);
                 await message.reply(reply);
@@ -45,7 +55,7 @@ const messageCreateHandler = async function(message){
         else if (config.ai_included_channels.includes(channelId)){
             brain.learn({
                 id: message.id,
-                content: message.content ?? "", // @ts-ignore
+                content: cleanMsg(message) || "", // @ts-ignore
                 authorId: message.author.id,
                 replyToId: message.reference?.messageId ?? null,
                 timestamp: message.createdTimestamp,
